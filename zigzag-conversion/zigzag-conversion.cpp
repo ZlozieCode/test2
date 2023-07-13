ea03981f48a8fe81e7cad99b570eed02cbbedbb6
Zlozie Code <92716726+ZlozieCode@users.noreply.github.com>
2023-07-13 17:03:21 +0530

    Time: 15 ms (42.65%) | Memory: 8.3 MB (76.39%) - LeetSync

--- zigzag-conversion/zigzag-conversion.cpp
+++ zigzag-conversion/zigzag-conversion.cpp
@@ -1,32 +1,26 @@
 class Solution {
 public:
-
     string convert(string s, int numRows) {
-    
-    if(numRows <= 1) return s;
-
-    vector<string>v(numRows, ""); 
-
-    int j = 0, dir = -1;
-
-    for(int i = 0; i < s.length(); i++)
-    {
-
-        if(j == numRows - 1 || j == 0) dir *= (-1); 
-		 
-        v[j] += s[i];
-
-        if(dir == 1) j++;
-
-        else j--;
-    }
-
-    string res;
-
-    for(auto &it : v) res += it; 
-
-    return res;
-
+        if(numRows==1)return s;
+        int n=s.size();
+        int gap = numRows - 1 + numRows -2;
+        string ans="";
+        int gap1 = gap;
+        int gap2 = -1;
+        for(int i=0;i<numRows;i++){
+            int g = i;
+            int j = 0;
+            while(g<n){
+                ans+=s[g];
+                if(j==0){
+                    g = g + (gap1==-1?gap2:gap1) + 1;j=1;
+                }else{
+                    g = g + (gap2==-1?gap1:gap2) + 1;j=0;
+                }
+            }
+            gap1-=2;
+            gap2+=2;
+        }
+        return ans;
     }
 };
-
