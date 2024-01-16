1a79eccb7358d731c3bca6afb64997bc1615b4fb
Zlozie Code <92716726+ZlozieCode@users.noreply.github.com>
2024-01-16 12:34:18 +0530

    Time: 8 ms (80.85%) | Memory: 11.3 MB (35.78%) - LeetSync

---
+++ 1-two-sum/two-sum.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+        int n=nums.size();
+        vector<int>v;
+        
+        unordered_map<int,int>m;
+        
+        for(int i=0;i<n;i++){
+            
+            if(m.find(target-nums[i])!=m.end()){
+                
+                v.push_back(m[target-nums[i]]);
+                v.push_back(i);
+                
+                return v;
+            }
+            
+            m[nums[i]]=i;
+        }
+        
+       
+        return v;
+        
+    }
+};
